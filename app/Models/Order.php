7e60acfa8b9671304fe7cdf28fbe2fe44f3a12a0
Miriam Dominguez <miriam.dominguez@metimur.mx>
2025-01-15 22:26:45 -0600

    Orders: procesos

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    // Relación con Empresas
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'empresa', 'id');
+    }
+
+    // Relación con Procesos
+    public function procesos()
+    {
+        return $this->hasMany(Models\Process::class, 'ot', 'id');
+    }
+}
